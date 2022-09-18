02015b3510c3e87a883f1d53b000e0b1312196d2
Phuong Thanh <lethi.phuongthanh.apu@gmail.com>
2022-09-18 21:47:24 +0700

    upload

---
+++ Web/app/model/ProductList.js
@@ -0,0 +1,22 @@
+class ProductList {
+  arrproducts = [];
+  arrListCart = [];
+
+  
+
+  addCart(item) {
+    this.arrListCart.push(item);
+  }
+
+  /* Xóa item trong giỏ hàng */
+  deleteItem(id) {
+  
+    this.arrListCart = this.arrListCart.filter((product) => product.id !== id);
+  }
+
+  updateItem(item) {
+     this.arrListCart = this.arrListCart.map((product) => {
+      return product.id === item.id ? item : product;
+    });
+  }
+}
